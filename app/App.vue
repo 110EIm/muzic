7c50b4fe537c8ed80075dc7cf62f85df72c881b7
MuRye <dreamaker7770@gmail.com>
2017-03-12 22:31:22 +0900

    Working

---
+++ app/App.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+	#app
+		section.section: .container
+			memo(v-for='memo in memos', :key='memo.music', :memo='memo')
+
+</template>
+
+<script>
+	import 'noto-sans-kr';
+	import 'bulma/css/bulma.css';
+	import 'babel-polyfill';
+
+	import Memo from './components/Memo.vue';
+
+	export default {
+		name: 'app',
+		components: { Memo },
+		data: () => ({
+			memos: []
+		}),
+
+		created() {
+			this.memos = [{name: '박성민', music: 'Anything'}, {name: 'Anonymous', music: 'HU'}];
+		},
+
+		methods: {
+			
+		}
+	};
+</script>
+
+<style>
+	* {
+		font-family: 'Noto Sans Korean', sans-serif;
+	}
+</style>
---
+++ app/components/Memo.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+	.box
+		.content: p
+			strong {{ memo.music }}
+			br
+			| {{ memo.name }}
+</template>
+
+<script>
+	export default {
+		props: ['memo']
+	}
+</script>
